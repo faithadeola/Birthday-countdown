958baf280775cfe7b9ea8aab746b49ccc8006b4a
Faith Adeola <faithadeola43@gmail.com>
2024-11-13 11:44:52 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const targetDate = new Date('June 14, 2025 00:00:00').getTime();
+
+function updateCountdown() {
+    const now = new Date().getTime();
+    const difference = targetDate - now;
+
+    const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+    document.getElementById('days').innerText = days;
+    document.getElementById('hours').innerText = hours;
+    document.getElementById('minutes').innerText = minutes;
+    document.getElementById('seconds').innerText = seconds;
+
+    if (difference < 0) {
+        clearInterval(countdownInterval);
+        document.getElementById('countdown').innerHTML = "Happy Birthday!";
+    }
+}
+
+
+const countdownInterval = setInterval(updateCountdown, 1000);
